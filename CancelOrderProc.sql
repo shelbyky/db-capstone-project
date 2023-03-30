7142aa43f46dd9e66265000a4fbaf58d355db844
Shelby Yancy <shelbyyancy@Shelbys-MacBook-Air-2.local>
2023-03-30 13:52:40 -0400

    Adding sql and screenshots with results for stored procedures and prepared statements

---
+++ CancelOrderProc.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE CancelOrder(order_id INT)
+BEGIN
+DELETE FROM Orders
+WHERE OrderID = order_id;
+SELECT CONCAT('Order ', @order_id, ' is cancelled') AS Confirmation;
+END //
---
+++ OrdersView.sql
@@ -0,0 +1,6 @@
+CREATE VIEW OrdersView AS
+SELECT OrderID,
+Quantity, 
+TotalCost AS Cost
+FROM Orders
+WHERE Quantity > 2;
